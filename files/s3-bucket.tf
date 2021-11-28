7fdea9ff0a573dc0ae7e85ce147e7d12f8bee4be
saravanan-ai <sarartr2759@gmail.com>
2021-11-28 15:39:36 +0000

    Added s3 bucket module

--- files/s3-bucket.tf
+++ files/s3-bucket.tf
@@ -1,5 +1,5 @@
 module "s3-bucket" {
   source  = "app.terraform.io/Saravananm-training/s3-bucket/aws"
   version = "2.2.0"
-  # insert required variables here
+  # insert required variables here only
 }
